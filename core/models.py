cbb0b3f159e2e61b8548eb5b3258d433d15dd713
Benjamin <kodiugos@gmail.com>
2024-04-24 20:28:47 -0500

    made project data model for users

---
+++ core/models.py
@@ -0,0 +1,38 @@
+from django.utils import timezone
+from django.db import models
+from .manager import CustomUserManager
+from django.contrib.auth.models import AbstractUser
+import uuid
+import os
+from django.conf import settings
+
+
+class CustomUser(AbstractUser):
+    GENDER_CHOICES = [
+        ('male', 'Male'),
+        ('female', 'Female'),
+    ]
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+        unique=True
+    )
+    username = models.CharField(unique=True, max_length=50, blank=False, null=False)
+    email = models.EmailField(unique=True, blank=False, null=False)
+    gender = models.CharField(max_length=10, choices=GENDER_CHOICES, null=True, blank=True)
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_email_verified = models.BooleanField(default=False)
+    paid_for_ad = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(default=timezone.now)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+
+    def __str__(self):
+        return self.username
