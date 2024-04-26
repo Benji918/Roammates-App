d8bd92e9a2614bf1f23edc23c16d0c02d2b097a2
Benjamin <kodiugos@gmail.com>
2024-04-26 14:12:34 -0500

    created ads data model

--- core/models.py
+++ core/models.py
@@ -7,6 +7,13 @@ import os
 from django.conf import settings
 
 
+def ad_image_file(instance, filename):
+    """Generate filename for new object image"""
+    ext = os.path.splitext(filename)[1]
+    filename = f'{uuid.uuid4()}{ext}'
+    return os.path.join('uploads', 'ad', filename)
+
+
 class CustomUser(AbstractUser):
     GENDER_CHOICES = [
         ('male', 'Male'),
@@ -36,3 +43,41 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+class Ad(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+        unique=True
+    )
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    no_rooms_for_rent = models.IntegerField(blank=False)
+    size_of_property = models.CharField(max_length=255)
+    address_of_property = models.CharField(max_length=255, unique=True, blank=False)
+    area_of_property = models.CharField(max_length=255, blank=False)
+    ROOM_AMENITIES_CHOICES = [
+        ('water', 'Water'),
+        ('furniture', 'Furniture'),
+        ('light', 'Light'),
+    ]
+    room_amenities = models.CharField(max_length=255, choices=ROOM_AMENITIES_CHOICES, blank=False)
+    cost_of_room = models.IntegerField(blank=False)
+    length_of_availability = models.DateField(blank=False)
+    phone_number = models.BigIntegerField(blank=False)
+    AD_TYPE_CHOICES = [
+        ('room to rent', 'Room to Rent'),
+        ('room wanted', 'Room Wanted'),
+        ('whole apartment for rent', 'whole apartment for rent'),
+    ]
+    ad_type = models.CharField(max_length=255, choices=AD_TYPE_CHOICES, blank=False)
+    ad_image = models.ImageField(upload_to=ad_image_file, null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Ad {self.address_of_property}"
+
+    def generate_sharable_link(self):
+        return f'/ad/{self.id}/'
