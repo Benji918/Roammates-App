158276f778aa73fa471ef7779aaf1f9750954b22
Benjamin <kodiugos@gmail.com>
2024-04-30 16:59:22 -0500

    added adimage model to django admin

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import CustomUser, Ad
+from .models import CustomUser, Ad, AdImage
 
 # Register your models here.
 admin.site.register(CustomUser)
 admin.site.register(Ad)
+admin.site.register(AdImage)
