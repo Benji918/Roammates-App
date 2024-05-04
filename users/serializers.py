0c7808f8dc1f00ba060020cbf6aac345d579b73f
Benjamin <kodiugos@gmail.com>
2024-05-04 13:51:58 -0500

    added custom validation to the username field

--- users/serializers.py
+++ users/serializers.py
@@ -1,12 +1,17 @@
 from djoser.serializers import UserSerializer, UserCreateSerializer as BaseUserSerializer
+from django.core.exceptions import ValidationError
 
 
 class UserCreateSerializer(BaseUserSerializer):
     class Meta(BaseUserSerializer.Meta):
         fields = ['id', 'email', 'username', 'password']
 
+    def validate(self, value):
+        if len(value) < 5:
+            raise ValidationError('The username must be at least 5 characters long.')
+
+
 
 class CurrentUserSerializer(UserSerializer):
     class Meta(UserSerializer.Meta):
         fields = ['id', 'email', 'username']
-
