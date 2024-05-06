15145d99af3fcc397a3073396c6f4933970993e4
Benjamin <kodiugos@gmail.com>
2024-05-06 03:18:21 -0500

    file changes

--- ads/urls.py
+++ ads/urls.py
@@ -1,9 +1,10 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from .views import AdViewSets, AdsSharableView
+from .views import AdViewSets, AdsSharableView, PaymentViewSets
 
 router = DefaultRouter()
 router.register('', AdViewSets)
+router.register('payment', PaymentViewSets, basename='payment')
 
 app_name = 'ads'
 
