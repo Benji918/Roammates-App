518bf0fc3ffa30a43c99f13eae85433c23a1ef51
Benjamin <kodiugos@gmail.com>
2024-05-07 01:48:28 -0500

    merged the adpayment model with ad model

--- ads/views.py
+++ ads/views.py
@@ -1,14 +1,17 @@
+import uuid
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
+import requests
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.generics import GenericAPIView
-from django.urls import reverse
+from django.conf import settings
 from rest_framework import viewsets, mixins, status
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated, AllowAny
-from core.models import Ad, AdImage
+from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
+from core.models import Ad
 from .serializers import AdsSerializer, AdImageSerializer
 from .permissions import IsOwnerOfAd
 from .filter import AdsFilter
@@ -75,3 +78,81 @@ class AdsSharableView(mixins.RetrieveModelMixin, GenericAPIView):
         ad = get_object_or_404(Ad, id=pk)
         sharable_link = ad.generate_sharable_link()
         return JsonResponse({'sharable_link': sharable_link})
+
+
+def initiate_payment(amount, email, ad_id):
+    url = "https://api.flutterwave.com/v3/payments"
+    headers = {
+        "Authorization": f"Bearer {settings.FLW_SEC_KEY}"
+
+    }
+
+    data = {
+        "tx_ref": str(uuid.uuid4()),
+        "amount": str(amount),
+        "currency": "NGN",
+        "redirect_url": "http:/127.0.0.1:8000/ads/payment/confirm_payment/?ad_id=" + ad_id,
+        "meta": {
+            "consumer_id": 23,
+            "consumer_mac": "92a3-912ba-1192a"
+        },
+        "customer": {
+            "email": email,
+            "phonenumber": "080****4528",
+            "name": "Yemi Desola"
+        },
+        "customizations": {
+            "title": "Pied Piper Payments",
+            "logo": "http://www.piedpiper.com/app/themes/joystick-v27/images/logo.png"
+        }
+    }
+
+    try:
+        response = requests.post(url, headers=headers, json=data)
+        response_data = response.json()
+        return Response(response_data)
+
+    except requests.exceptions.RequestException as err:
+        print("The payment didn't go through!!!")
+        return Response({"error": str(err)}, status=500)
+
+
+class PaymentViewSets(mixins.RetrieveModelMixin, mixins.DestroyModelMixin, mixins.ListModelMixin, mixins.UpdateModelMixin, viewsets.GenericViewSet):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated, IsOwnerOfAd]
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_staff:
+            return Ad.objects.all()
+        return Ad.objects.filter(user=user)
+
+    def get_permissions(self):
+        if self.request.method in ["PATCH", "DELETE"]:
+            return [IsAdminUser()]
+        return [IsAuthenticated()]
+
+    http_method_names = ["get", "patch", "post", "delete", "options", "head"]
+
+    @action(detail=True, methods=['POST'])
+    def pay(self, request, pk):
+        ad = self.get_object()
+        amount = ad.ad_cost
+        email = request.user.email
+        ad_id = str(ad.id)
+        return initiate_payment(amount, email, ad_id)
+
+    @action(detail=False, methods=["POST"])
+    def confirm_payment(self, request):
+        ad_id = request.GET.get("ad_id")
+        print(ad_id)
+        ad = Ad.objects.get(id=ad_id)
+        ad.pending_status = "C"
+        ad.save()
+        serializer = AdsSerializer(ad)
+
+        data = {
+            "msg": "Payment was successful",
+            "data": serializer.data
+        }
+        return Response(data)
